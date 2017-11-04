364d4b76362d691edee9bfea3b9b0788e60e2f47
Sören Tempel <soeren+git@soeren-tempel.net>
2017-11-04 14:14:10 +0100

    feucht.h: Doxygen comments for functions

--- include/feucht.h
+++ include/feucht.h
@@ -10,6 +10,26 @@
 	#define FEUCHT_INTERVAL 60
 #endif
 
+/**
+ * Initialize the protocol for a protocol server located at the given
+ * remote address.
+ *
+ * @param remote Address of the protocol server.
+ * @return Negative errno value on failure and zero on success.
+ */
 int init_protocol(ipv6_addr_t *remote);
+
+/**
+ * Free all resources associated with the protocol.
+ */
 void free_protocol(void);
+
+/**
+ * Transmit a new value to the protocol server.
+ *
+ * @param buf Pointer to a memory location containing the data which
+ *	should be transmitted to the server.
+ * @param count Amount of bytes that should be transmitted.
+ * @return Negative errno value on failure and zero on success.
+ */
 int update_humidity(char *buf, size_t count);
